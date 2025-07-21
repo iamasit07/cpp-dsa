8928633c71de144bfdecd4b3a92b01b907154e5f
Asit <asit.upadhyay793@gmail.com>
2025-07-21 21:05:17 +0530

    recusions topic

---
+++ recursion/josephus-problem.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int josephusProblem(int n, int k) {
+	if (n == 1)
+		return 0;
+	return (josephusProblem(n - 1, k) + k) % n;
+}
+
+int main() {
+	cout << josephusProblem(5, 3);
+	return 0;
+}
--- recursion/tower-of-hanoi.cpp
+++ recursion/tower-of-hanoi.cpp
@@ -1,1 +1,18 @@
-tower-of-hanoi.cpp
+#include<bits/stdc++.h>
+using namespace std;
+
+void towerOfHanoi(int n, char a, char b, char c) {
+	if (n == 1) {
+		cout << "Move 1 from " << a << " to " << c << endl;
+		return;
+	}
+
+	towerOfHanoi(n - 1, a, c, b);
+	cout << "Move " << n << " from " << a << " to " << c << endl;
+	towerOfHanoi(n - 1, b, a, c);
+}
+
+int main() {
+	towerOfHanoi(3, 'A', 'B', 'C');
+	return 0;
+}
