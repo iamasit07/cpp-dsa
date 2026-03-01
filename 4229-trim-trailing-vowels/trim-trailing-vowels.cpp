5aa38c738340e1586a73611c990a67df5a3f37b6
Asit <121558316+iamasit07@users.noreply.github.com>
2026-03-01 16:46:34 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (53.19%) - LeetSync

---
+++ 4229-trim-trailing-vowels/trim-trailing-vowels.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string trimTrailingVowels(string s) {
+        string ans = "";
+        int i = s.length() - 1;
+        while (i >= 0 and (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' ||
+                          s[i] == 'o' || s[i] == 'u'))
+            i--;
+        return s.substr(0, (i < 0 ? 0 : i + 1));
+    }
+};
