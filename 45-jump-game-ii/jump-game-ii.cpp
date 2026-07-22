20b8fda6cc6431986f50ef3a5dfc0a3a9069032d
Asit <121558316+iamasit07@users.noreply.github.com>
2026-07-22 20:24:10 +0530

    Time: 0 ms (100.00%) | Memory: 20.6 MB (68.57%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,29 +1,24 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        int i = 0, count = 0, n = nums.size();
+        int n = nums.size();
         if (n == 1)
             return 0;
 
-        while (i < n) {
-            int x = nums[i];
-            if (i + x + 1 >= n) {
-                count++;
-                break;
-            }
+        int jump = 0, farthest = 0, curr_end = 0;
+        for (int i = 0; i < n - 1; i++) {
+            if (i + nums[i] > farthest)
+                farthest = i + nums[i];
+            if (i == curr_end) {
+                jump++;
+                curr_end = farthest;
+                farthest = 0;
 
-            int minv = INT_MAX, itr = 0;
-            for (int it = i + 1; it <= min(n, x + i); it++) {
-                if (n - it - 1 - nums[it] <= minv) {
-                    minv = n - it - nums[it] - 1;
-                    itr = it;
-                }
+                if (curr_end >= n - 1)
+                    break;
             }
-
-            i = itr;
-            count++;
         }
 
-        return count;
+        return jump;
     }
 };
