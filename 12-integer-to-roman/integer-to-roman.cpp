b1c73a74e49a3f0236ca804efc3c966bd62b59a6
Asit <121558316+iamasit07@users.noreply.github.com>
2026-07-29 13:24:03 +0530

    Time: 7 ms (28.60%) | Memory: 12.8 MB (18.84%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.cpp
+++ 12-integer-to-roman/integer-to-roman.cpp
@@ -1,13 +1,13 @@
 class Solution {
-public:
+private:
     string repeat(int n, string s) {
         string ans = "";
-        while (n > 0) {
+        while (n--)
             ans += s;
-            n--;
-        }
         return ans;
     }
+
+public:
     string intToRoman(int num) {
         vector<pair<int, string>> a = {
             {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"}, {100, "C"},
@@ -18,13 +18,7 @@ public:
         int x = 0;
         while (x < a.size() and num > 0) {
             int s = num / a[x].first;
-            string temp = "";
-
-            if (s <= 3)
-                temp = repeat(s, a[x].second);
-            else
-                temp = a[x].second + a[x - 1].second;
-            ans += temp;
+            ans += repeat(s, a[x].second);
             num %= a[x].first;
             x++;
         }
