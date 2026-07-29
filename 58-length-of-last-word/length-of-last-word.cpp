e2b0a973573122f0ba102e413ef5bb5cf8e7f448
Asit <121558316+iamasit07@users.noreply.github.com>
2026-07-29 14:44:07 +0530

    Time: 4 ms (0.49%) | Memory: 9.3 MB (5.83%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.cpp
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -1,12 +1,26 @@
 class Solution {
+private:
+    string fun(string s) {
+        string ans = "";
+        for (int i = 0; i < (s.length() - 1); i++) {
+            if (s[i] == ' ' and s[i + 1] == ' ')
+                continue;
+            else
+                ans += s[i];
+        }
+        if (s.back() != ' ')
+            ans += s.back();
+
+        return ans;
+    }
+
 public:
     int lengthOfLastWord(string s) {
-        regex pattern(R"(\s+)");
-        string filter = regex_replace(s, pattern, " ");
-        int start = 0, n = filter.size();
+        string filter = fun(s);
+        int start = 0, n = filter.length();
         if (filter[n - 1] == ' ')
             filter.pop_back();
-        n = filter.size();
+        n = filter.length();
         for (int i = 0; i < (n - 1); i++) {
             if (filter[i] == ' ' and filter[i + 1] != ' ')
                 start = i + 1;
