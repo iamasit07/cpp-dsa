ce63092206d4296a45674bb69e557a7fba17645d
Asit <121558316+iamasit07@users.noreply.github.com>
2026-07-29 13:51:40 +0530

    Time: 0 ms (100.00%) | Memory: 22.3 MB (76.54%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -1,18 +1,27 @@
 class Solution {
 public:
     int candy(vector<int>& ratings) {
-        int n = ratings.size();
-        vector<int> a(n, 1);
-
+        int up = 0, down = 0, peak = 0;
+        int total = 1, n = ratings.size();
         for (int i = 1; i < n; i++) {
-            if (ratings[i] > ratings[i - 1])
-                a[i] = a[i - 1] + 1;
+            if (ratings[i] == ratings[i - 1]) {
+                up = 0;
+                down = 0;
+                peak = 0;
+                total++;
+            } else if (ratings[i] > ratings[i - 1]) {
+                up++;
+                down = 0;
+                peak = up + 1;
+                total += peak;
+            } else {
+                up = 0;
+                down++;
+                total += down;
+                if (down >= peak)
+                    total++;
+            }
         }
-        for (int i = n - 2; i >= 0; i--) {
-            if (ratings[i] > ratings[i + 1])
-                a[i] = max(a[i + 1] + 1, a[i]);
-        }
-
-        return accumulate(a.begin(), a.end(), 0);
+        return total;
     }
 };
