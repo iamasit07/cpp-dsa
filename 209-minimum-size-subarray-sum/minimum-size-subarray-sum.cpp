9f748d711416ee31a40f76b99ae268f26be5a1ec
Asit <121558316+iamasit07@users.noreply.github.com>
2026-08-01 10:59:05 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (31.97%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
@@ -1,30 +1,16 @@
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
-        int start = 0, end = 0, sum = 0;
-        int n = nums.size();
-        while (end < n and sum < target) {
-            sum += nums[end];
-            end++;
-        }
+        int left = 0, sum = 0, result = INT_MAX, n = nums.size();
+        for (int right = 0; right < n; right++) {
+            sum += nums[right];
 
-        if (sum < target)
-            return 0;
-
-        int ans = end--;
-        while (start <= end and end < n) {
-            cout << sum << " " << start << " " << end << '\n';
-            if (sum >= target) {
-                ans = min(ans, (end - start + 1));
-                sum -= nums[start];
-                start++;
-            } else {
-                end++;
-                if (end < n)
-                    sum += nums[end];
+            while (left <= right and sum >= target) {
+                result = min(result, right - left + 1);
+                sum -= nums[left++];
             }
         }
 
-        return ans;
+        return (result == INT_MAX ? 0 : result);
     }
 };
