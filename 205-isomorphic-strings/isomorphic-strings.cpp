139755dede8425431a4a5c899a1bcfa05d1d60ef
Asit <121558316+iamasit07@users.noreply.github.com>
2026-08-05 13:34:42 +0530

    Time: 1 ms (54.86%) | Memory: 9.3 MB (58.59%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -5,26 +5,15 @@ public:
             return false;
 
         int n = s.length();
-        map<char, vector<int>> a, b;
-        for (int i = 0; i < n; i++)
-            a[s[i]].push_back(i);
-        for (int i = 0; i < n; i++)
-            b[t[i]].push_back(i);
-
-        map<char, bool> visited;
+        map<char, char> m1, m2;
         for (int i = 0; i < n; i++) {
-            if (visited[s[i]])
-                continue;
-
-            if (a[s[i]].size() != b[t[i]].size())
+            if (m1.find(s[i]) != m1.end() and m1[s[i]] != t[i])
+                return false;
+            if (m2.find(t[i]) != m2.end() and m2[t[i]] != s[i])
                 return false;
 
-            vector<int> x = a[s[i]];
-            vector<int> y = b[t[i]];
-            for (int i = 0; i < x.size(); i++) {
-                if (x[i] != y[i])
-                    return false;
-            }
+            m1[s[i]] = t[i];
+            m2[t[i]] = s[i];
         }
 
         return true;
