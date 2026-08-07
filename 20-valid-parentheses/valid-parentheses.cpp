f7b6b111912c37017cd5006314a3f95e44e8815a
Asit <121558316+iamasit07@users.noreply.github.com>
2026-08-07 22:24:50 +0530

    Time: 2 ms (14.86%) | Memory: 10.3 MB (6.57%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> st;
+        stack<int> st;
         for (auto i : s) {
-            if (i == '(' or i == '[' or i == '{')
+            if (i == '(' or i == '{' or i == '[')
                 st.push(i);
-            else if (!st.empty()) {
-                if (i == ')' and st.top() == '(')
-                    st.pop();
-                else if (i == ']' and st.top() == '[')
-                    st.pop();
-                else if (i == '}' and st.top() == '{')
-                    st.pop();
-                else
-                    return false;
-            } else
-                return false;
+            else if (st.empty())
+                st.push(i);
+            else if (i == ')' and st.top() == '(')
+                st.pop();
+            else if (i == '}' and st.top() == '{')
+                st.pop();
+            else if (i == ']' and st.top() == '[')
+                st.pop();
+            else
+                st.push(i);
         }
-
         return st.empty();
     }
 };
