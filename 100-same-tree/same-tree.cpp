086386d24c4da58876df5c433c77787e6018d7f7
Asit <121558316+iamasit07@users.noreply.github.com>
2026-09-04 12:29:11 +0530

    Time: 0 ms (100.00%) | Memory: 12.8 MB (47.71%) - LeetSync

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -15,29 +15,10 @@ public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
         if (p == nullptr and q == nullptr)
             return true;
-
-        queue<TreeNode*> q1, q2;
-        q1.push(p);
-        q2.push(q);
-        while (!q1.empty() and !q2.empty()) {
-            TreeNode *t1 = nullptr, *t2 = nullptr;
-            while (!q1.empty() and !q2.empty()) {
-                t1 = q1.front();
-                t2 = q2.front();
-                q1.pop(), q2.pop();
-                if (t1 == nullptr and t2 == nullptr)
-                    continue;
-                else if (t1 == nullptr or t2 == nullptr)
-                    return false;
-                else if (t1->val != t2->val)
-                    return false;
-                q1.push(t1->left);
-                q1.push(t1->right);
-                q2.push(t2->left);
-                q2.push(t2->right);
-            }
-        }
-
-        return true;
+        if (p == nullptr or q == nullptr)
+            return false;
+        if (p->val != q->val)
+            return false;
+        return (isSameTree(p->left, q->left) && isSameTree(p->right, q->right));
     }
 };
